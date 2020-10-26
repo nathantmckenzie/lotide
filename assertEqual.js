0a7584db1a25418ac6304e917b981102f3c08cf6
Nathan McKenzie <nathantaylormckenzie@hotmail.com>
2020-10-26 23:16:20 +0000

    assert function

---
+++ assertEqual.js
@@ -0,0 +1,16 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("something","something");
+assertEqual("onething","anotherthing");
+assertEqual(123,123);
+assertEqual(13123,2342);
