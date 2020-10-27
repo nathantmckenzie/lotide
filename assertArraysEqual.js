7a7ca4ae69157dd33b3a4e5cf578ab0a4b2ae3c0
Nathan McKenzie <nathantaylormckenzie@hotmail.com>
2020-10-27 23:09:40 +0000

    compares two arrays and includes a message

---
+++ assertArraysEqual.js
@@ -0,0 +1,15 @@
+// FUNCTION IMPLEMENTATION
+const assertArrayEqual = function(actual, expected) {
+    if (JSON.stringify(actual)==JSON.stringify(expected)) {
+      console.log(`Assertion Passed: ${actual} === ${expected}`);
+    } else if (JSON.stringify(actual)!==JSON.stringify(expected)) {
+      console.log(`Assertion Failed: ${actual} !== ${expected}`);
+    }
+  };
+  
+  
+  // TEST CODE
+  console.log(assertArrayEqual([1, 1, 0],[1, 0, 1]));
+  console.log(assertArrayEqual([1, 1],[1,1]));
+  console.log(assertArrayEqual(["some text"],["some text"]));
+  
