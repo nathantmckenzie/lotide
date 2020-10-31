ac5ab01f630e735e8b66020295117f04436a586d
Nathan McKenzie <nathantaylormckenzie@hotmail.com>
2020-10-31 00:36:15 +0000

    find a specific key

---
+++ findKey.js
@@ -0,0 +1,41 @@
+const assertEqual = function(actual, expected) {
+    if (actual === expected) {
+      console.log(`Assertion Passed: ${actual} === ${expected}`);
+    } else {
+      console.log(`Assertion Failed: ${actual} !== ${expected}`);
+    }
+  };
+  
+  
+  const findKey = function(object, callback) {
+    for (let star in object) {
+      console.log();
+      if (callback(object[star]) === true) {
+        return star;
+      }
+    }
+    return undefined;
+  };
+  
+  
+  assertEqual(findKey({
+    "Blue Hill": {
+      stars: 1
+    },
+    "Akaleri": {
+      stars: 3
+    },
+    "noma": {
+      stars: 2
+    },
+    "elBulli": {
+      stars: 3
+    },
+    "Ora": {
+      stars: 2
+    },
+    "Akelarre": {
+      stars: 3
+    }
+  }, x => x.stars === 2), "noma"); // => "noma"
+  
