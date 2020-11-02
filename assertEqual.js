47d98b0869641933454c42c4971c598f341324c0
Nathan McKenzie <nathanmckenzie@Nathans-MacBook-Air.local>
2020-11-02 15:39:56 -0800

    refactored code

--- assertEqual.js
+++ assertEqual.js
@@ -7,10 +7,5 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("something","something");
-assertEqual("onething","anotherthing");
-assertEqual(123,123);
-assertEqual(13123,2342);
+
+module.exports = assertEqual;
