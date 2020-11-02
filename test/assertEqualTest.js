60b93fe0a8bafbcf7fc4a7bdb003e53f579f9c03
Nathan McKenzie <nathanmckenzie@Nathans-MacBook-Air.local>
2020-11-02 15:44:10 -0800

    test for assertEqual function

---
+++ test/assertEqualTest.js
@@ -0,0 +1,11 @@
+const assertEqual = require('../assertEqual');
+
+
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("something","something");
+assertEqual("onething","anotherthing");
+assertEqual(123,123);
+assertEqual(13123,2342);
