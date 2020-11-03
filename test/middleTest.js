df9fe6652c2b06318532a8a6af03cff99ab9f009
Nathan McKenzie <nathanmckenzie@Nathans-MacBook-Air.local>
2020-11-03 12:27:57 -0800

    mocha test

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,11 +1,9 @@
 const middle = require("../middle");
 const assertArrayEqual = require("../assertArraysEqual");
+const assertEqual = require('../assertEqual');
 
 
 
-//assertArrayEqual(middle([2]),[]);
-//assertArrayEqual(middle([2, 3, 5, 11, 9, 10, 22]),[11]);
-//assertArrayEqual(middle([2, 3, 4, 2, 5, 6]),[4,2]);
 
 describe("#middle", () => {
     it("returns [11] for [2, 3, 5, 11, 9, 10, 22]", () => {
