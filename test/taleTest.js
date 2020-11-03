506d41230ac37e966b16d790c4734fa576a9b918
Nathan McKenzie <nathantaylormckenzie@hotmail.com>
2020-11-03 16:48:02 +0000

    testing with mocha

--- test/taleTest.js
+++ test/taleTest.js
@@ -1,6 +1,19 @@
 const tail = require("../tail");
 const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
+
+//assertEqual(tail(words), [ 'Lighthouse', 'Labs' ]);
+//assertEqual(tail(nothing.length, 0));
 
 
-assertEqual(tail(words), [ 'Lighthouse', 'Labs' ]);
-assertEqual(tail(nothing.length, 0));
+describe("#head", () => {
+    it("returns [ 'Lighthouse', 'Labs' ] for ['YoYo','Lighthouse','Labs']", () => {
+      assert.strictEqual(tail(['YoYo','Lighthouse','Labs']), [ 'Lighthouse', 'Labs' ]);
+    });
+  });
+
+  it("returns 'something' for ['something']", () => {
+    assert.strictEqual(tail(['something']), 'something');
+  });
+
+  
