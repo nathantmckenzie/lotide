f5a478d647294f4f0ab35115d02b65ff9c9d5259
Nathan McKenzie <nathantaylormckenzie@hotmail.com>
2020-11-03 17:04:40 +0000

    testing with mocha

--- test/middleTest.js
+++ test/middleTest.js
@@ -3,6 +3,16 @@ const assertArrayEqual = require("../assertArraysEqual");
 
 
 
-assertArrayEqual(middle([2]),[]);
-assertArrayEqual(middle([2, 3, 5, 11, 9, 10, 22]),[11]);
-assertArrayEqual(middle([2, 3, 4, 2, 5, 6]),[4,2]);
+//assertArrayEqual(middle([2]),[]);
+//assertArrayEqual(middle([2, 3, 5, 11, 9, 10, 22]),[11]);
+//assertArrayEqual(middle([2, 3, 4, 2, 5, 6]),[4,2]);
+
+describe("#middle", () => {
+    it("returns [11] for [2, 3, 5, 11, 9, 10, 22]", () => {
+      assert.strictEqual(middle([2, 3, 5, 11, 9, 10, 22]), [11]);
+    });
+  });
+
+  it("returns [4,2] for [2, 3, 4, 2, 5, 6]", () => {
+    assert.strictEqual(middle([2, 3, 4, 2, 5, 6]), [4,2]);
+  });
